d2ccc360929706ceacfceb1de00cedd33a40ba55
prayaggheeson <prayag.gheeson@gmail.com>
2023-11-01 16:57:49 +0530

    updated connectbutton Components

--- src/Components/ConnectButton.jsx
+++ src/Components/ConnectButton.jsx
@@ -1,18 +1,17 @@
-import { useWeb3Modal,useAccount } from "@web3modal/wagmi/react";
+import { useWeb3Modal } from "@web3modal/wagmi/react";
 
 export default function ConnectButton() {
   const { open } = useWeb3Modal();
-  const { address, isConnecting, isDisconnected } = useAccount()
+//   const { address, isConnecting, isDisconnected } = useAccount()
 
-  if (isConnecting) return <div>Connecting…</div>
-  if (isDisconnected) return <div>Disconnected</div>
-  return <div>{address}</div>
+//   if (isConnecting) return <div>Connecting…</div>
+//   if (isDisconnected) return <div>Disconnected</div>
 
   return (
     <>
-      <button onClick={() => open()}>Open Connect Modal</button>
+      <button onClick={() => open()}>Connect Wallet</button>
       <button onClick={() => open({ view: "Networks" })}>
-        Open Network Modal
+        Switch Network 
       </button>
     </>
   );
